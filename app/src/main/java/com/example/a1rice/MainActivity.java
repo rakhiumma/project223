39f135a5ae11435e401b5ae56c02408d0fdbdfe7
Rakhi15 <mail2k.rakhi@gmail.com>
2022-03-04 20:32:19 +0530

    new dp 04

--- app/src/main/java/com/example/a1rice/MainActivity.java
+++ app/src/main/java/com/example/a1rice/MainActivity.java
@@ -2,18 +2,20 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
  public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
     }
