1d2428d823653c2227bca9dec79ddfcf168e6a6a
Rakhi15 <mail2k.rakhi@gmail.com>
2022-03-05 19:23:35 +0530

    new dp 05

--- app/src/main/java/com/example/a1rice/MainActivity.java
+++ app/src/main/java/com/example/a1rice/MainActivity.java
@@ -2,10 +2,12 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Display;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,10 +15,19 @@ import android.widget.ImageView;
 
  public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        }, 4000);
 
     }
 }
--- app/src/main/java/com/example/a1rice/SigninActivity.java
+++ app/src/main/java/com/example/a1rice/SigninActivity.java
@@ -1,14 +1,30 @@
 package com.example.a1rice;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class SigninActivity extends AppCompatActivity {
 
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+    }
+
+    public void open_diag(View view) {
+
+        progressDialog=new ProgressDialog(SigninActivity.this);
+        progressDialog.show();
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setContentView(R.layout.loading_rice);
+        progressDialog.getWindow().setBackgroundDrawableResource(
+                android.R.color.transparent);
+
     }
 }
