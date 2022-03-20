7b8a635cc4e65413aa046da2be00be93308765ea
rakhi15 <mail2k.rakhi@gmail.com>
2022-03-20 11:58:28 +0530

    1158032022

---
+++ app/src/main/java/com/oakspro/a1rice/AddressData.java
@@ -0,0 +1,61 @@
+package com.oakspro.a1rice;
+
+public class AddressData {
+    String address_id, address_name, address_type, address_info, address_pincode, address_area, address_city;
+
+    public String getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(String address_id) {
+        this.address_id = address_id;
+    }
+
+    public String getAddress_name() {
+        return address_name;
+    }
+
+    public void setAddress_name(String address_name) {
+        this.address_name = address_name;
+    }
+
+    public String getAddress_type() {
+        return address_type;
+    }
+
+    public void setAddress_type(String address_type) {
+        this.address_type = address_type;
+    }
+
+    public String getAddress_info() {
+        return address_info;
+    }
+
+    public void setAddress_info(String address_info) {
+        this.address_info = address_info;
+    }
+
+    public String getAddress_pincode() {
+        return address_pincode;
+    }
+
+    public void setAddress_pincode(String address_pincode) {
+        this.address_pincode = address_pincode;
+    }
+
+    public String getAddress_area() {
+        return address_area;
+    }
+
+    public void setAddress_area(String address_area) {
+        this.address_area = address_area;
+    }
+
+    public String getAddress_city() {
+        return address_city;
+    }
+
+    public void setAddress_city(String address_city) {
+        this.address_city = address_city;
+    }
+}
